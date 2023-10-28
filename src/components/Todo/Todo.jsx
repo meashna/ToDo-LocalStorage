5e172e7f80f46a31860369cf9afa95432358bcdf
meashna <cs25ashn@kmeacollege.ac.in>
2023-10-28 15:00:54 +0530

    logout button added

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Todo.css'
+import { useNavigate } from 'react-router-dom'
 
 const Todo = () => {
+  const navigate = useNavigate()
+  const logout=()=>{
+    navigate('/register')
+  }
   return (
     <div>
         <div className="todo">
             <h1>Sucessfully Logined!</h1>
-            
+            <button onClick={logout}>Logout</button>
         </div>
     </div>
   )
